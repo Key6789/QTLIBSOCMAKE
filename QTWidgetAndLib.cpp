3cd5cbe4c0a663411006d35af02f62becf70d484
@Worker <2308902576@DESKTOP-P6KI5GE>
2024-09-14 16:33:38 +0800

    @edit release 与 debug的编译 linux 与window

--- QTWidgetAndLib.cpp
+++ QTWidgetAndLib.cpp
@@ -3,6 +3,9 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include "TcpCommunication.h"
+
+
+#define __QString(str) QString::fromLocal8Bit(str)
 QTWidgetAndLib::QTWidgetAndLib(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::QTWidgetAndLib)
@@ -18,14 +21,14 @@ QTWidgetAndLib::QTWidgetAndLib(QWidget *parent)
     layout->addWidget(new QLabel("标签"));
 
     setCentralWidget(widget);*/
-    CommunicationModule::TcpCommunication* tcpComm = new CommunicationModule::TcpCommunication("127.0.0.1",8080,this);
+   CommunicationModule::TcpCommunication* tcpComm = new CommunicationModule::TcpCommunication("127.0.0.1",8080,this);
    QWidget* widget = new QWidget(this);
    QVBoxLayout* layout = new QVBoxLayout(widget);
    layout->addWidget(tcpComm->showConnectStatus(widget));
    layout->addWidget(tcpComm->showSendData(widget));
    layout->addWidget(tcpComm->showReceiveData(widget));
    layout->addWidget(tcpComm->showError(widget));
-   layout->addWidget(new QLabel("标签"));
+   layout->addWidget(new QLabel(__QString("标签")));
    setCentralWidget(widget);
 }
 
